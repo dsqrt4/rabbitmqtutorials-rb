b65767bc59e04356f467864e80ced4b5ccedca9f
dsqrt4 <44970945+dsqrt4@users.noreply.github.com>
2021-06-13 18:37:19 +0200

    switch to rspec

---
+++ spec/rabbitmqtutorials_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RabbitMqTutorials do
+  it "has a version number" do
+    expect(RabbitMqTutorials::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
